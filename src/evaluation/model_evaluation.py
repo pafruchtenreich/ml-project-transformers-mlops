a36e2f7ead7b177f6c52d1feec6e55da99a73ec6
Paul-Antoine Fruchtenreich <paul-antoine.fruchtenreich@ens.psl.eu>
2025-02-20 12:26:33 +0100

    create evaluation function

--- src/evaluation/model_evaluation.py
+++ src/evaluation/model_evaluation.py
@@ -1,61 +1,25 @@
-import torch
-from torch.utils.data import DataLoader, TensorDataset
-from tqdm import tqdm
-from transformers import BertTokenizer
+import evaluate
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
+from src.setup_logger import setup_logger
 
 
-def generate_summaries_transformer(model, batch_size, tokenized_input, limit=None):
+def evaluate_model(data, predictions):
     """
-    Generates summaries using a Transformer model.
+    Evaluate the model using ROUGE metrics.
 
     Parameters:
-    - model (torch.nn.Module): The pre-trained Transformer model.
-    - batch_size (int): Number of samples per batch.
-    - tokenized_input (torch.Tensor): Tokenized input articles.
-    - limit (int or None): Maximum number of batches to process. If None, processes all batches.
+    - data (pd.DataFrame): The dataset used for evaluation.
+    - predictions (List[str]): The generated summaries.
 
     Returns:
-    - List of generated summaries.
+    - None
     """
-    model.to(device)
-    model.eval()
-    dataset = TensorDataset(tokenized_input)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-    predictions_transformer = []
-    with torch.no_grad():  # Disable gradient calculation for inference
-        for i, batch in enumerate(
-            tqdm(dataloader, desc="Processing Batches", unit="batch")
-        ):
-            if limit is not None and i == limit:  # Stop after reaching the limit
-                break
+    logger = setup_logger()
+    rouge = evaluate.load("rouge")
 
-            tokenized_inputs = batch[0].to(device)
-            current_batch_size = tokenized_inputs.size(0)
-            start_summaries = torch.zeros((current_batch_size, 128), device=device)
-            start_summaries[:, 0] = 101  # [CLS] token
+    data.loc[:, "predictions_transformer"] = predictions
 
-            for k in range(0, 127):
-                output = model(tokenized_inputs.long(), start_summaries.long())
-                start_summaries[:, k + 1] = output.argmax(dim=-1)[:, k].detach()
-                if (
-                    start_summaries[:, k + 1] == 102
-                ).all():  # Stop if all sequences end with [SEP]
-                    break
-
-            for start_summary in start_summaries:
-                summary = tokenizer.decode(
-                    start_summary.long(), skip_special_tokens=True
-                )
-                predictions_transformer.append(summary)
-
-    return predictions_transformer
+    reference_summaries = list(data["Summary"])
+    results = rouge.compute(predictions=predictions, references=reference_summaries)
+    logger.info(f"ROUGE metrics: {results}")
