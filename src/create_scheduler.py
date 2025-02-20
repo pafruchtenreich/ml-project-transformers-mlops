2fbc4361b6e43c245cb22e85764d62ed68efe798
Paul-Antoine Fruchtenreich <paul-antoine.fruchtenreich@ens.psl.eu>
2025-02-20 11:49:07 +0100

    create scheduler function

---
+++ src/create_scheduler.py
@@ -0,0 +1,22 @@
+from transformers import get_linear_schedule_with_warmup
+
+
+def create_scheduler(dataloader, optimizer, n_epochs):
+    """
+    Create a scheduler for the optimizer.
+
+    Parameters:
+    - dataloader: DataLoader that contains training data
+    - optimizer: Optimizer used to update the model's weights
+    - n_epochs: Number of epochs to train the model
+
+    Returns:
+    - scheduler: Scheduler for the optimizer
+    """
+    num_train_steps = len(dataloader) * n_epochs
+    warmup_steps = int(0.1 * num_train_steps)
+
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=warmup_steps, num_training_steps=num_train_steps
+    )
+    return scheduler
