2ea44027bd14d9cfa769671ed900e6a3c31cad6b
jpeignon <julien.peignon@ensae.fr>
2025-04-06 18:31:35 +0000

    remove a value in gridsearch

--- src/models/train_models.py
+++ src/models/train_models.py
@@ -275,7 +275,7 @@ def finetune_model_with_gridsearch_cv(
 
     # Define a grid of hyperparameters we want to search
     param_grid = {
-        "learning_rate": [1e-5, 5e-6, 1e-6],
+        "learning_rate": [5e-6, 1e-6],
         "weight_decay": [0.0, 1e-2],
         "label_smoothing": [0.0, 0.1],
     }
