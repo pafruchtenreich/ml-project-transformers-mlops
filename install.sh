026025925be0e24b484cb06eb2bdc18cf396754a
gamer35 <florian.basso@ensae.fr>
2025-04-17 10:05:57 +0000

    added spacy component to download before running model

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@
 apt-get -y update
 apt-get install -y python3-pip python3-venv
 
+
 # Create empty virtual environment
 python3 -m venv summary_generation
 source summary_generation/bin/activate
 
 # Install project dependencies
 pip install -r requirements.txt
+
+# Install model
+python -m spacy download en_core_web_sm
